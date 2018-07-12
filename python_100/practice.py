bfe03c1242dc0ee3bfadd836d3fd52ecb83159e4
YolenSong <s986984996@gmail.com>
2018-07-12 21:47:10 +0800

    second python

--- python_100/practice.py
+++ python_100/practice.py
@@ -64,11 +64,53 @@ def area(radius):
 print(area(2))
 '''
 
+'''
 def sayHello(name):
     print("Welcome",name)
 
 sayHello("zyk")
+'''
 
+#可变参数
+'''
+def changeme(mylist):
+    "修改已有列表"
+    mylist.append([1,2,3,4])
+    print("函数内",mylist)
+    return
+
+list = [12,23,34]
+changeme(list)
+print("函数外",list)
+'''
+
+#指定参数
+'''
+def jiacheng(a,b,c):
+    print("a",a)
+    print("b",b)
+    print("c",c)
+
+a = "aaa"
+b = "bbb"
+c = "ccc"
+
+jiacheng(a,b,c)
+jiacheng(a=b,b=c,c=a)
+'''
+
+
+#缺省函数
+def showInfo(name,age=23):
+    print("name",name)
+    print("age",age)
+
+name1 = "jack"
+age1 = 34
+
+name2 = "Bob"
+showInfo(name1,age1)
+showInfo(name2)
 
 
 
